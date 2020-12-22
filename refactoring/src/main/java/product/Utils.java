5fe09f3f8d0d57d45cc72a2ef7513dd7e1bea31a
merak1t <donatsokolov99@gmail.com>
2020-12-22 03:24:34 +0300

    add Product & Utils

---
+++ refactoring/src/main/java/product/Utils.java
@@ -0,0 +1,28 @@
+package product;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Utils {
+
+    public static Connection getConnection() throws SQLException {
+        String DB_URL = "jdbc:sqlite:test.db";
+        return DriverManager.getConnection(DB_URL);
+    }
+
+    public static void createTables() throws SQLException {
+        try (Connection c = getConnection()) {
+            String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        }
+    }
+
+}
