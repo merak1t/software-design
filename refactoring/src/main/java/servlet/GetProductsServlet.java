cb91ce5d14f50110b50be2decf8188ba92b47fe6
merak1t <donatsokolov99@gmail.com>
2020-12-22 03:30:33 +0300

    add Abstract Servlet

--- refactoring/src/main/java/servlet/GetProductsServlet.java
+++ refactoring/src/main/java/servlet/GetProductsServlet.java
@@ -1,10 +1,9 @@
 package servlet;
+
 import product.ProductDao;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 import static product.ProductHTML.printProductsHTML;
 
@@ -12,23 +11,15 @@ import static product.ProductHTML.printProductsHTML;
 /**
  * @author akirakozov
  */
-public class GetProductsServlet extends HttpServlet {
-
-    private final ProductDao productDao;
+public class GetProductsServlet extends AbstractProductServlet {
 
     public GetProductsServlet(ProductDao productDao) {
-        this.productDao = productDao;
+        super(productDao);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
-            printProductsHTML(productDao.getProducts(), response.getWriter());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        printProductsHTML(productDao.getProducts(), response.getWriter());
     }
+
 }
